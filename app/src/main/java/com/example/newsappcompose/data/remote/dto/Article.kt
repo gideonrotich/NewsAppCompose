e268f98ccd108dd038a9336794a720a8c7c1cd0a
kanake10 <10kanake@gmail.com>
2022-08-31 09:42:09 +0300

    null pointers

--- app/src/main/java/com/example/newsappcompose/data/remote/dto/Article.kt
+++ app/src/main/java/com/example/newsappcompose/data/remote/dto/Article.kt
@@ -3,14 +3,14 @@ package com.example.newsappcompose.data.remote.dto
 import com.example.newsappcompose.domain.models.News
 
 data class Article(
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
 )
 
     fun Article.toNews():News{
--- app/src/main/java/com/example/newsappcompose/domain/models/News.kt
+++ app/src/main/java/com/example/newsappcompose/domain/models/News.kt
@@ -1,11 +1,11 @@
 package com.example.newsappcompose.domain.models
 
 data class News(
-    val author: String,
-    val content: String,
-    val description: String,
-    val title: String,
-    val url: String,
-    val source:String,
-    val urlToImage: String
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val title: String?,
+    val url: String?,
+    val source:String?,
+    val urlToImage: String?
 )
